36cdf9c89702a8080a890272b1d5d74f9ad92028
mSinisa <msinisa602@yahoo.com>
2019-01-12 11:28:26 +0100

    refactored 2 functions into 1

--- senate.js
+++ senate.js
@@ -181,27 +181,10 @@ var app = new Vue({
             return filtered;
         },
 
-        hideAndShowText: function () {
-            var dots = document.getElementById("dots");
-            var moreText = document.getElementById("moreText");
-            var btnText = document.getElementById("textButton");
-            btnText.addEventListener("click", function () {
-                if (dots.style.display === "none") {
-                    dots.style.display = "inline";
-                    btnText.innerHTML = "+ Read more";
-                    moreText.style.display = "none";
-                } else {
-                    dots.style.display = "none";
-                    btnText.innerHTML = "- Read less";
-                    moreText.style.display = "inline";
-                }
-            });
-        },
-
-        hideAndShowText2: function () {
-            var dots = document.getElementById("dots2");
-            var moreText = document.getElementById("moreText2");
-            var btnText = document.getElementById("textButton2");
+        hideAndShowText: function (id1, id2, id3) {
+            var dots = document.getElementById(id1);
+            var moreText = document.getElementById(id2);
+            var btnText = document.getElementById(id3);
             btnText.addEventListener("click", function () {
                 if (dots.style.display === "none") {
                     dots.style.display = "inline";
